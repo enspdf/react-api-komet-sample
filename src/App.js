4544b77276bada096316968746fe47aa6fb19b56
Sebastian Higuita <sebastianhc@kometsales.com>
2020-10-23 10:50:51 -0500

    Use an external API to fetch data basis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [isSuccess, setIsSuccess] = useState(null);
+
+  const getProducts = () => {
+    fetch("https://nodejs-komet-api.herokuapp.com/api/product")
+      .then(res => res.json())
+      .then(({ success, products }) => {
+        setProducts(products);
+        setIsSuccess(success);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products?.map(product => (
+            <tr key={product._id}>
+              <td>{product._id}</td>
+              <td>{product.name}</td>
+              <td>{product.description}</td>
+              <td>{product.price}</td>
+              <td>
+                <a onClick={() => console.log(product)}>Edit</a>
+                <a onClick={() => console.log(product._id)}>Delete</a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }

a60491c455654abf4828b64a509a9a54c8281222
danielcifuentes54 <danielcifuentes.54@hotmail.com>
2020-10-29 16:40:24 -0500

    Add loading in app.js (#2)
    Co-authored-by: Daniel Cifuentes <dcifuentes@kometsales.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [isSuccess, setIsSuccess] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getProducts = () => {
+    setIsLoading(true);
     fetch("https://nodejs-komet-api.herokuapp.com/api/product")
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(({ success, products }) => {
         setProducts(products);
         setIsSuccess(success);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   useEffect(() => {
@@ -21,31 +26,35 @@ function App() {
 
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products?.map(product => (
-            <tr key={product._id}>
-              <td>{product._id}</td>
-              <td>{product.name}</td>
-              <td>{product.description}</td>
-              <td>{product.price}</td>
-              <td>
-                <a onClick={() => console.log(product)}>Edit</a>
-                <a onClick={() => console.log(product._id)}>Delete</a>
-              </td>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {products?.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+                <td>
+                  <a onClick={() => console.log(product)}>Edit</a>
+                  <a onClick={() => console.log(product._id)}>Delete</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }

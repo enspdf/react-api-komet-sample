64dcad941ff9f5bca132a9f5f0a07001ee2e86a1
danielcifuentes54 <danielcifuentes.54@hotmail.com>
2020-10-30 10:39:21 -0500

    add reac router dom (#3)
    Co-authored-by: Daniel Cifuentes <dcifuentes@kometsales.com>

--- src/App.js
+++ src/App.js
@@ -1,60 +1,40 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Product from "./components/Product";
+import About from "./components/About";
+import Home from "./components/Home";
+
 import "./App.css";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [isSuccess, setIsSuccess] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const getProducts = () => {
-    setIsLoading(true);
-    fetch("https://nodejs-komet-api.herokuapp.com/api/product")
-      .then((res) => res.json())
-      .then(({ success, products }) => {
-        setProducts(products);
-        setIsSuccess(success);
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
   return (
-    <div className="App">
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products?.map((product) => (
-              <tr key={product._id}>
-                <td>{product._id}</td>
-                <td>{product.name}</td>
-                <td>{product.description}</td>
-                <td>{product.price}</td>
-                <td>
-                  <a onClick={() => console.log(product)}>Edit</a>
-                  <a onClick={() => console.log(product._id)}>Delete</a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/product">Product</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/product">
+            <Product />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Product.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import "../App.css";
+
+function Product() {
+  const [products, setProducts] = useState([]);
+  const [isSuccess, setIsSuccess] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getProducts = () => {
+    setIsLoading(true);
+    fetch("https://nodejs-komet-api.herokuapp.com/api/product")
+      .then((res) => res.json())
+      .then(({ success, products }) => {
+        setProducts(products);
+        setIsSuccess(success);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div className="App">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products?.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+                <td>
+                  <a onClick={() => console.log(product)}>Edit</a>
+                  <a onClick={() => console.log(product._id)}>Delete</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default Product;
